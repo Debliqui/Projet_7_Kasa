043d8134ac6912ebe6f73ca367ca525c96e6e875
Debliqui Valentin <debliqui.valentin@outlook.com>
2024-11-25 15:43:35 -0500

    Aajout de comtenu et de style au header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,10 @@
 import KasaFooter from '../../assets/images/kasa_white.svg'
-import '../../utils/style/pages/Footer.scss'
 
 export default function Footer() {
   const copirite = '© 2020 Kasa. All rights reserved'
   return (
     <footer className="footer">
-      <img
-        className="footer__kasa_white"
-        src={KasaFooter}
-        alt="Kasa avec la premiere lettre a en forme de maison"
-      />
+      <img className="footer__kasa_white" src={KasaFooter} alt="" />
       <p>{copirite}</p>
     </footer>
   )
