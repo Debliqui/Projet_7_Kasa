bfe32266024003951de63edb2c0ec6b9ce78f168
Debliqui Valentin <debliqui.valentin@outlook.com>
2024-11-25 10:26:34 -0500

    Ajout du router et les différentes pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
+import APropos from './pages/APropos/APropos'
+import Error from './components/Error/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/a-propos" element={<APropos />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
