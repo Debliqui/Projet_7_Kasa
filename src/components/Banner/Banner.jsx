434763d964ffbb4f7574f8b6c1e3fb82ca90b070
Debliqui Valentin <debliqui.valentin@outlook.com>
2024-11-25 17:29:12 -0500

    Creation de la banniere d'accueil

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+  return (
+    <section className="banner">
+      <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
+    </section>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,17 @@
+import Banner from '../../components/Banner/Banner.jsx'
+import styled from 'styled-components'
+
+const HomeContainer = styled.main`
+  max-width: 1240px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`
+
 export default function Home() {
-  return <h1>Chez vous, partout et ailleurs</h1>
+  return (
+    <HomeContainer>
+      <Banner />
+    </HomeContainer>
+  )
 }
